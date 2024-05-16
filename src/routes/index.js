c01ec13877edac578d9aefee277acb5484cc51bf
Johnny Telles <johnnytelles77@gmail.com>
2024-05-16 08:14:42 -0400

    Entrega 1 Mongo

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,6 @@ import cartsRouters from "./carts.routes.js"
 const router = Router();
 
 router.use("/products", productsRouters); /// Ruta de los prodcutos
-router.use("/", cartsRouters);  /// Ruta de los carritos
+router.use("/carts", cartsRouters);  /// Ruta de los carritos
 
 export default router;
